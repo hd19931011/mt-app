3d4aaaa202594bac6e4c296e26f82b29631744fd
hd <hd@.com>
2019-05-24 20:20:38 +0800

    'add'

--- src/main.js
+++ src/main.js
@@ -3,13 +3,30 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
-
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
+import '@/assets/css/main.css'
+import store from './store'
+// import axios from './axios';
+Vue.use(ElementUI)
 Vue.config.productionTip = false
-
+Vue.directive('document-click', {
+  bind (el, binding, vnode) {
+    document.addEventListener('click', binding.value, false)
+  },
+  inserted () {
+    console.log('insert')
+  },
+  update () {
+    console.log('update')
+  }
+})
 /* eslint-disable no-new */
+// Vue.prototype.axios = axios;
 new Vue({
   el: '#app',
   router,
+  store,
   components: { App },
   template: '<App/>'
 })
